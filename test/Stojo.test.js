4ceb448be041189aa4bf64cf699e247c349fb299
brainswax <bwdenton@gmail.com>
2021-07-17 16:19:34 -0700

    Added tests

--- test/Stojo.test.js
+++ test/Stojo.test.js
@@ -1,22 +1,44 @@
 /* eslint-env jest */
 import * as stojo from '../lib/Stojo.mjs'
 import fs from 'fs'
+import _ from 'lodash'
 
-describe('Test the store functionality', () => {
+describe('Stojo', () => {
   const file = './stojo.test.sqlite3'
   if (fs.existsSync(file)) fs.unlinkSync(file)
 
-  const db = new stojo.Stojo({ file: file })
+  const store = new stojo.Stojo({ file: file })
+  const key = 'test.foo'
+  const object = { foo: 'foo' }
+  const notakey = 'not.an.object'
+  const nullkey = 'null'
 
-  it('can store and retrieve an object', async () => {
-    return db.init()
-      .then(() => db.store('test.foo', { foo: 'foo' }))
-      .then(() => db.fetch('test.foo'))
-      .then((data) => {
-        expect(data).toBeDefined()
-        expect(data.foo).toBeDefined()
-        expect(data.foo).toBe('foo')
+  it('can store an object', async () => {
+    return store.store(key, object)
+  })
+
+  it('can fetch the stored object', async () => {
+    return store.fetch(key)
+      .then(data => {
+        expect(_.isEqual(data, object)).toBe(true)
       })
-      .then(() => db.close())
+  })
+
+  it('can handle fetching an object that doesn\'t exist', async () => {
+    return store.fetch(notakey)
+      .then(data => {
+        expect(data).toBe(null)
+      })
+  })
+
+  it('can store a null object', async () => {
+    return store.store(nullkey, null)
+  })
+
+  it('can fetch a null object', async () => {
+    return store.fetch(nullkey)
+      .then(data => {
+        expect(data).toBe(null)
+      })
   })
 })
