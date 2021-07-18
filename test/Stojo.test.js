a51ba5e20df2ff54ea9b76078e21230401f8514c
brainswax <brains@codegrill.org>
2021-07-17 20:37:11 -0700

    Mocha and Scoped Module (#1)
    * Switch to mocha and add more tests
    * Initial documentation
    * Scoped @codegrill module

--- test/Stojo.test.js
+++ test/Stojo.test.js
@@ -1,44 +1,61 @@
 /* eslint-env jest */
-import * as stojo from '../lib/index.mjs'
+import { Stojo } from '../lib/index.mjs'
 import fs from 'fs'
 import _ from 'lodash'
+import assert from 'assert'
 
 describe('Stojo', () => {
   const file = './stojo.test.sqlite3'
   if (fs.existsSync(file)) fs.unlinkSync(file)
 
-  const store = new stojo.Stojo({ file: file })
+  const store = new Stojo({ file: file })
   const key = 'test.foo'
   const object = { foo: 'foo' }
   const notakey = 'not.an.object'
   const nullkey = 'null'
 
-  it('can store an object', async () => {
-    return store.store(key, object)
+  describe('init', () => {
+    it('can initialize', () => {
+      return store.init()
+    })
   })
 
-  it('can fetch the stored object', async () => {
-    return store.fetch(key)
-      .then(data => {
-        expect(_.isEqual(data, object)).toBe(true)
-      })
+  describe('store', () => {
+    it('can store an object', async () => {
+      return store.store(key, object)
+    })
+
+    it('can store a null object', async () => {
+      return store.store(nullkey, null)
+    })
   })
 
-  it('can handle fetching an object that doesn\'t exist', async () => {
-    return store.fetch(notakey)
-      .then(data => {
-        expect(data).toBe(null)
-      })
+  describe('fetch', () => {
+    it('can fetch a stored object', async () => {
+      return store.fetch(key)
+        .then(data => {
+          assert.ok(_.isEqual(data, object))
+        })
+    })
+
+    it('can fetch a null object', async () => {
+      return store.fetch(nullkey)
+        .then(data => {
+          assert.equal(data, null)
+        })
+    })
+
+    it('can handle fetching an object that doesn\'t exist', async () => {
+      return store.fetch(notakey)
+        .then(data => {
+          assert.equal(data, null)
+        })
+    })
   })
 
-  it('can store a null object', async () => {
-    return store.store(nullkey, null)
-  })
-
-  it('can fetch a null object', async () => {
-    return store.fetch(nullkey)
-      .then(data => {
-        expect(data).toBe(null)
-      })
+  describe('close', () => {
+    it('can close the database connection', () => {
+      return store.close()
+    })
   })
 })
